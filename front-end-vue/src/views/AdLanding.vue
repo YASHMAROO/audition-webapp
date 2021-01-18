1d3205a72d114c6732528e63c45e036c29fb47a2
error-200 <yashv8696@gmail.com>
2021-01-19 01:01:11 +0530

    [+] Admin landing and usercontrol UX changed

--- front-end-vue/src/views/AdLanding.vue
+++ front-end-vue/src/views/AdLanding.vue
@@ -1,208 +1,266 @@
 <template>
-  <v-app>
-    <v-content>
-      <v-card class="mx-auto overflow-hidden">
-        <v-app-bar>
-          <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+	<v-app>
+		<v-content>
+			<v-card class="mx-auto overflow-hidden">
+				<v-app-bar>
+					<v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-          <v-toolbar-title>GLUG AUDITIONS 2020</v-toolbar-title>
-          <v-spacer></v-spacer><v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
+					<!-- <v-toolbar-title>GLUG AUDITIONS 2020</v-toolbar-title> -->
+					<v-spacer></v-spacer><v-spacer></v-spacer>
+					<v-spacer></v-spacer>
+					<v-spacer></v-spacer>
+					<v-spacer></v-spacer>
+					<v-spacer></v-spacer>
+					<v-spacer></v-spacer>
+					<v-spacer></v-spacer>
+					<v-spacer></v-spacer>
+					<v-spacer></v-spacer>
+					<v-spacer></v-spacer>
 
-          <v-btn @click="logout" color="light-blue darken-1">Logout</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn @click="purge" color="light-blue darken-1">PURGE</v-btn>
-        </v-app-bar>
-      </v-card>
+					<v-btn @click="logout" color="#00FFFF">Logout</v-btn>
+					<v-spacer></v-spacer>
+					<!-- <v-btn @click="purge" color="light-blue darken-1">PURGE</v-btn> -->
+				</v-app-bar>
+			</v-card>
 
-      <v-navigation-drawer v-model="drawer" absolute temporary>
-        <v-list nav dense>
-          <v-list-item-group
-            v-model="group"
-            active-class="deep-purple--text text--lighten-2"
-          >
-            <router-link tag="span" to="/">
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>mdi-home</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Home</v-list-item-title>
-              </v-list-item>
-            </router-link>
-            <v-list-item>
-              <v-switch v-model="darkmode" label="Dark Mode"></v-switch>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
+			<v-navigation-drawer v-model="drawer" absolute temporary>
+				<v-list nav dense>
+					<v-list-item-group
+						v-model="group"
+						active-class="deep-purple--text text--lighten-2"
+					>
+						<router-link tag="span" to="/">
+							<v-list-item>
+								<v-list-item-icon>
+									<v-icon>mdi-home</v-icon>
+								</v-list-item-icon>
+								<v-list-item-title>Home</v-list-item-title>
+							</v-list-item>
+						</router-link>
+						<v-list-item>
+							<v-switch v-model="darkmode" label="Dark Mode"></v-switch>
+						</v-list-item>
+					</v-list-item-group>
+				</v-list>
+			</v-navigation-drawer>
 
-      <v-container fluid>
-        <div>
-          <v-spacer>
-            <h1>Welcome, {{ adminUser }}</h1>
-          </v-spacer>
-        </div>
-        <v-spacer />
-        <v-spacer />
-        <v-container absolute fluid>
-          <v-row align="center" justify="center">
-            <v-col cols="12" sm="6" lg="3">
-              <v-card class="mx-auto" outlined color="#2196F3">
-                <v-list-item three-line>
-                  <v-list-item-content>
-                    <div class="subtitle-2">TOTAL STUDENTS</div>
-                    <v-list-item-title class="display-2">{{
-                      items.length
-                    }}</v-list-item-title>
-                  </v-list-item-content>
+			<v-container fluid>
+				<div>
+					<v-spacer>
+            <v-alert
+      outlined
+      color="#00FFFF"
+    >
+     <h1 class="text-center">STUDENTS DASHBOARD</h1>
+     
+    </v-alert>
+					</v-spacer>
+				</div>
+				<v-spacer />
+				<v-spacer />
+				<v-container absolute fluid>
+					<v-row align="center" justify="center">
+						<v-col cols="12" sm="6" lg="3">
+							<v-alert outlined color="#00FFFF">
+								<v-list-item three-line>
+									<v-list-item-content>
+										<div class="subtitle-2">TOTAL STUDENTS</div>
+										<v-list-item-title class="display-2">{{
+											items.length
+										}}</v-list-item-title>
+									</v-list-item-content>
 
-                  <v-list-item-avatar tile size="90">
-                    <v-img src="../assets/img/stu.png"></v-img>
-                  </v-list-item-avatar>
-                </v-list-item>
-              </v-card>
-            </v-col>
-            <v-col cols="12" sm="6" lg="3">
-              <v-card class="mx-auto" outlined color="#76FF03">
-                <v-list-item three-line>
-                  <v-list-item-content>
-                    <div class="subtitle-2">COMPLETED</div>
-                    <v-list-item-title class="display-2"></v-list-item-title>
-                    <v-list-item-subtitle>vvv</v-list-item-subtitle>
-                  </v-list-item-content>
+									<v-list-item-avatar tile size="90">
+										<v-img src="../assets/img/stu.png"></v-img>
+									</v-list-item-avatar>
+								</v-list-item>
+							</v-alert>
+						</v-col>
+						<v-col cols="12" sm="6" lg="3">
+								<v-alert outlined color="success">
+								<v-list-item three-line>
+									<v-list-item-content>
+										<div class="subtitle-2">COMPLETED</div>
+										<v-list-item-title class="display-2"></v-list-item-title>
+										<v-list-item-subtitle></v-list-item-subtitle>
+									</v-list-item-content>
 
-                  <v-list-item-avatar tile size="90">
-                    <v-img src="../assets/img/tick.png"></v-img>
-                  </v-list-item-avatar>
-                </v-list-item>
-              </v-card>
-            </v-col>
-            <v-col cols="12" sm="6" lg="3">
-              <v-card class="mx-auto" outlined color="#FFAB00">
-                <v-list-item three-line>
-                  <v-list-item-content>
-                    <div class="subtitle-2">PENDING</div>
-                    <v-list-item-title class="display-2"></v-list-item-title>
-                  </v-list-item-content>
+									<v-list-item-avatar tile size="90">
+										<v-img src="../assets/img/t.png"></v-img>
+									</v-list-item-avatar>
+								</v-list-item>
+								</v-alert>
+						</v-col>
+						<v-col cols="12" sm="6" lg="3">
+								<v-alert outlined color="warning">
+								<v-list-item three-line>
+									<v-list-item-content>
+										<div class="subtitle-2">PENDING</div>
+										<v-list-item-title class="display-2"></v-list-item-title>
+									</v-list-item-content>
 
-                  <v-list-item-avatar tile size="90">
-                    <v-img src="../assets/img/pend.png"></v-img>
-                  </v-list-item-avatar>
-                </v-list-item>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
+									<v-list-item-avatar tile size="90">
+										<v-img src="../assets/img/w.png"></v-img>
+									</v-list-item-avatar>
+								</v-list-item>
+								</v-alert>
+						</v-col>
+					</v-row>
+				</v-container>
 
-        <v-data-table
-          :headers="headers"
-          hide-default-footer
-          :items="items"
-          class="elevation-1"
-        >
-          <template v-slot:item="row">
-            <tr>
-              <td>{{ row.item.name }}</td>
-              <td>{{ row.item.selected.status }}</td>
-              <td>{{ row.item.final.status }}</td>
-              <td>{{ row.item.lastUser }}</td>
-              <td>{{ row.item.selected.user }}</td>
-              <td>
-                <v-btn
-                  class="mx-2"
-                  light
-                  small
-                  @click="usercontrol(row.item)"
-                  >Detail</v-btn
-                >
-              </td>
-            </tr>
-          </template>
-        </v-data-table>
-      </v-container>
-    </v-content>
-  </v-app>
+				<!-- <v-data-table
+					:headers="headers"
+					hide-default-footer
+					:items="items"
+					class="elevation-1"
+				>
+					<template v-slot:item="row">
+						<tr @click="usercontrol(row.item)">
+							<td>{{ row.item.name }}</td>
+							<td>{{ row.item.selected.status }}</td>
+							<td>{{ row.item.final.status }}</td>
+							<td>{{ row.item.lastUser }}</td>
+							<td>{{ row.item.selected.user }}</td>
+							<td>
+								<v-btn class="mx-2" light small @click="usercontrol(row.item)"
+									>Detail</v-btn
+								>
+							</td>
+						</tr>
+					</template>
+				</v-data-table> -->
+				<template>
+					<v-card>
+						<v-tabs
+							v-model="tab"
+							color="cyan"
+							
+							dark
+							icons-and-text
+						>
+							<v-tabs-slider></v-tabs-slider>
+
+							<v-tab href="#subscribe">
+								ROUND 1
+							
+							</v-tab>
+
+							<v-tab href="#contact">
+								ROUND 2
+								
+							</v-tab>
+						</v-tabs>
+
+						<v-tabs-items v-model="tab">
+							<v-tab-item :key="1" value="subscribe">
+								<v-data-table
+									:headers="headers"
+									hide-default-footer
+									:items="items"
+									class="elevation-1"
+								>
+									<template v-slot:item="row">
+										<tr @click="usercontrol(row.item)">
+											<td>{{ row.item.name }}</td>
+											<td>{{ row.item.selected.status }}</td>
+											<td>{{ row.item.final.status }}</td>
+											<td>{{ row.item.lastUser }}</td>
+											<td>{{ row.item.selected.user }}</td>
+											<td>
+												<v-btn class="mx-2" light small @click="usercontrol(row.item)"
+													>Detail</v-btn
+												>
+											</td>
+										</tr>
+									</template>
+								</v-data-table>
+							</v-tab-item>
+							<v-tab-item :key="2" value="contact">
+								<v-card flat>
+									<v-card-text>contact</v-card-text>
+								</v-card>
+							</v-tab-item>
+						</v-tabs-items>
+					</v-card>
+				</template>
+			</v-container>
+		</v-content>
+	</v-app>
 </template>
 
 <script>
 import common from "@/services/common.js";
 export default {
-  name: "Landing",
-  components: {},
+	name: "Landing",
+	components: {},
 
-  data() {
-    return {
-      drawer: false,
-      adminUser: localStorage.getItem("admin"),
-      items: [],
-      expand: false,
-      darkmode: false,
-      headers: [
-        { text: "FULL NAME", align: "start", value: "name" },
-        { text: "STATUS", align: "start", value: "status" },
-        { text: "FINAL STATUS", align: "start", value: "final status" },
-        { text: "LAST VIEWED", align: "start", value: "details" },
-        { text: "LAST CHANGED", align: "start", value: "details" },
-        { text: "DETAILS", align: "start", value: "details" },
-      ],
-    };
-  },
-  watch: {
-    darkmode(newvalue) {
-      this.$vuetify.theme.dark = newvalue;
-    },
-  },
-  created() {
-    if (localStorage.getItem("token") === null) {
-      localStorage.clear();
-      this.$router.push("/");
-    } else {
-      common.getUsers().then((res) => {
-        if (res.status === 200) {
-          this.items = res.data.doc;
-        } else if (res.status === 401) {
-          alert("UNAUTHORISED ACCESS");
-          localStorage.clear("token");
-          this.$router.push("/");
-        } else {
-          alert("No data");
-        }
-      });
-    }
-    this.$vuetify.theme.dark = true;
-  },
-  methods: {
-    logout() {
-      common.logout().then((res) => {
-        localStorage.clear();
-        alert(res.data);
-        this.$router.push("/AdLog");
-      });
-    },
-    usercontrol(a) {
-      var payload = a;
-      payload['lastUser']=this.adminUser 
-      common.updateEntry(payload)
-      let routeData = this.$router.resolve({
-        name: "UserControl",
-        query: { id: a._id },
-      });
-      window.open(routeData.href, "_blank");
-    },
-    purge() {
-      common.purge().then((res) => {
-        alert(res.data.message);
-        location.reload();
-      });
-    },
-  },
+	data() {
+		return {
+			drawer: false,
+			adminUser: localStorage.getItem("admin"),
+			items: [],
+			expand: false,
+			darkmode: false,
+			headers: [
+				{ text: "FULL NAME", align: "start", value: "name" },
+				{ text: "STATUS", align: "start", value: "status" },
+				{ text: "FINAL STATUS", align: "start", value: "final status" },
+				{ text: "LAST VIEWED", align: "start", value: "details" },
+				{ text: "LAST CHANGED", align: "start", value: "details" },
+				{ text: "DETAILS", align: "start", value: "details" }
+			],
+			tab: null,
+			rounds: ["web", "shopping", "videos", "images", "news"]
+		};
+	},
+	watch: {
+		darkmode(newvalue) {
+			this.$vuetify.theme.dark = newvalue;
+		}
+	},
+	created() {
+		if (localStorage.getItem("token") === null) {
+			localStorage.clear();
+			this.$router.push("/");
+		} else {
+			common.getUsers().then(res => {
+				if (res.status === 200) {
+					this.items = res.data.doc;
+				} else if (res.status === 401) {
+					alert("UNAUTHORISED ACCESS");
+					localStorage.clear("token");
+					this.$router.push("/");
+				} else {
+					alert("No data");
+				}
+			});
+		}
+		this.$vuetify.theme.dark = true;
+	},
+	methods: {
+		logout() {
+			common.logout().then(res => {
+				localStorage.clear();
+				alert(res.data);
+				this.$router.push("/AdLog");
+			});
+		},
+		usercontrol(a) {
+			var payload = a;
+			payload["lastUser"] = this.adminUser;
+			common.updateEntry(payload);
+			let routeData = this.$router.resolve({
+				name: "UserControl",
+				query: { id: a._id }
+			});
+			window.open(routeData.href, "_blank");
+		},
+		purge() {
+			common.purge().then(res => {
+				alert(res.data.message);
+				location.reload();
+			});
+		}
+	}
 };
 </script>
--- front-end-vue/src/views/UserControl.vue
+++ front-end-vue/src/views/UserControl.vue
@@ -1,13 +1,13 @@
 <template>
 	<v-app>
-		<v-toolbar color="#141d2b" flat>
+		<!-- <v-toolbar color="#141d2b" flat>
 			<v-spacer />
 			<v-spacer />
 			<v-spacer />
 			<v-row justify="center">
 				<v-dialog v-model="dialog" persistent max-width="290">
 					<template v-slot:activator="{ on, attrs }">
-						<v-btn color="error" dark v-bind="attrs" v-on="on">
+						<v-btn color="#B2EBF2" dark v-bind="attrs" v-on="on">
 							Finalise
 						</v-btn>
 					</template>
@@ -27,28 +27,33 @@
 					</v-card>
 				</v-dialog>
 			</v-row>
-		</v-toolbar>
-		<v-spacer />
-		<v-spacer />
+		</v-toolbar> -->
 
 		<v-container fluid>
-			<v-alert text color="info"  icon="mdi-school">
-				<h4 class="headline" color="#9fef00">
+			<!-- <v-alert text color="info" icon="mdi-school">
+				<h4 class="headline" color="#B2EBF2">
 					<strong>
-						WELCOME TO THE DETAILS OF  {{ details.name.toUpperCase() }}
+						WELCOME TO THE DETAILS OF {{ details.name.toUpperCase() }}
 					</strong>
 				</h4>
-			</v-alert>
+			</v-alert> -->
+			<v-alert
+      outlined
+      color="#00FFFF"
+    >
+     <h2 class="text-center">WELCOME TO THE DETAILS OF {{ details.name.toUpperCase() }}</h2>
+     
+    </v-alert>
 		</v-container>
 		<v-container fluid>
 			<v-row>
 				<v-col md="6">
 					<v-card>
-						<v-toolbar color="info" dark flat>
+						<v-toolbar color="cyan" dark flat>
 							<v-toolbar-title color="info">Answers across all rounds</v-toolbar-title>
 
 							<template v-slot:extension>
-								<v-tabs v-model="tab" align-with-title color="#9fef00">
+								<v-tabs v-model="tab" align-with-title color="#B2EBF2">
 									<v-tabs-slider color="#9fef00"></v-tabs-slider>
 
 									<v-tab v-for="item in items" :key="item">
@@ -69,7 +74,17 @@
 				</v-col>
 				<v-col md="6">
 					<div>
-						<v-alert dense outlined type="error">
+						<!-- <template>
+							<v-container fluid>
+								<v-select
+									:items="status"
+									label="CURRENT STATUS"
+									outlined
+									color="#B2EBF2"
+								></v-select>
+							</v-container>
+						</template> -->
+						<!-- <v-alert dense outlined type="error">
 							<v-row align="center">
 								<v-col>
 									Current Round : {{ details.round }} Current selection status :
@@ -85,46 +100,62 @@
 									>
 								</v-col>
 							</v-row>
+						</v-alert> -->
+						<v-alert outlined color="#00FFFF">
+							<div class="title"></div>
+							<div>
+								<template>
+									<v-container fluid>
+										<v-select :items="status" label="SELECTION STATUS" solo></v-select>
+									</v-container>
+								</template>
+							</div>
+							<template>
+								<v-expansion-panels color="#B2EBF2" inset>
+									<v-expansion-panel>
+										<v-expansion-panel-header>
+											SUBMIT FEEDBACK
+										</v-expansion-panel-header>
+										<v-expansion-panel-content>
+											<v-row align="center" justify="center">
+												<v-col>
+													<v-textarea
+														v-model="feedback"
+														label="FEEDBACK"
+														name="feedback"
+														outlined
+														color="#00FFFF"
+														type="text"
+													></v-textarea>
+
+													<v-card-actions>
+														<v-spacer />
+														<v-btn @click="submitFeedback" color="#00FFFF">SUBMIT</v-btn>
+													</v-card-actions>
+												</v-col>
+											</v-row>
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+								</v-expansion-panels>
+							</template>
 						</v-alert>
-						<v-container fluid class="fill-height">
-							<v-row align="center" justify="center">
-								<v-col>
-									<v-card class="elevation-12">
-										<v-toolbar color="primary" dark flat>
-											<v-spacer></v-spacer>
-											<v-toolbar-title> Your Feedback </v-toolbar-title>
-											<v-spacer />
-										</v-toolbar>
-										<v-spacer />
-										<v-card-text>
-											<v-textarea
-												v-model="feedback"
-												label="YOUR feedback"
-												name="feedback"
-												outlined
-												type="text"
-											></v-textarea>
-										</v-card-text>
-										<v-card-actions>
-											<v-spacer />
-											<v-btn @click="submitFeedback" color="teal darken-3"
-												>Post your feedback</v-btn
-											>
-										</v-card-actions>
-									</v-card>
-								</v-col>
-							</v-row>
-							<v-container fluid>
+						<v-alert outlined color="#00FFFF">
+							<div class="title">
+								FEEDBACKS
+								
+							</div>
+								
+							<div>
 								<v-data-iterator
 									:items="details.feedback"
 									item-key="_id"
 									:single-expand="expand"
 								>
-									<template v-slot:header>
+									<!-- <template v-slot:header>
 										<v-alert text outlined color="orange" icon="mdi-fire">
-										FEEDBACK
+											FEEDBACK
 										</v-alert>
-									</template>
+									</template> -->
 									<template v-slot:default="{ items }">
 										<v-row>
 											<v-col
@@ -132,37 +163,36 @@
 												:key="item._id"
 												cols="40"
 												sm="12"
-												lg="6"
+												
 											>
-												<v-card>
+												<v-alert outlined color="#00FFFF">
+													<v-list-item-content>{{ item.feedback }}</v-list-item-content>
+													<v-divider class="my-4 info" style="opacity: 0.22"></v-divider>
+													<v-list-item-content>
+														<v-list-item-title> - {{ item.user }}</v-list-item-title>
+													</v-list-item-content>
+												</v-alert>
+												<!-- <v-card>
 													<v-card-title>
-														<h5> 	{{ item.feedback }}</h5>
+														<h5>{{ item.feedback }}</h5>
 													</v-card-title>
 
 													<v-divider class="mx-4"></v-divider>
-													<!-- <v-list dense>
+													<v-list dense>
 														<v-container fluid color="deep-purple lighten-2">
 														Feed-back by {{ item.user }} in Round {{ item.round }}
 														</v-container>
-													</v-list> -->
+													</v-list>
 													<v-card-actions>
-														<v-btn
-														color="#9fef00"
-														text
-														
-														>
-														- {{ item.user }}
-														</v-btn>
-														</v-card-actions>
-
-												</v-card>
-
+														<v-btn color="#9fef00" text> - {{ item.user }} </v-btn>
+													</v-card-actions>
+												</v-card> -->
 											</v-col>
 										</v-row>
 									</template>
 								</v-data-iterator>
-							</v-container>
-						</v-container>
+							</div>
+						</v-alert>
 					</div>
 				</v-col>
 			</v-row>
@@ -184,7 +214,8 @@ export default {
 			expand: false,
 			type: null,
 			dialog: false,
-			items: ["ROUND1", "ROUND2"]
+			items: ["ROUND1", "ROUND2"],
+			status: ["SELECTED", "FURTHER REVIEW", "REJECTED"]
 		};
 	},
 	beforeCreate() {
@@ -245,5 +276,3 @@ export default {
 	}
 };
 </script>
-
-
